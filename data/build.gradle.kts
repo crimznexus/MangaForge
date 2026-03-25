ea733ea3b506c0bbb2babf182e43433ec2a332d1
Muhammad Faizan Mustafa <crimznexus>
2026-03-25 16:00:24 +0500

    Initial MangaForge (Mihon base)

---
+++ data/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(mihonx.plugins.android.library)
+    alias(mihonx.plugins.spotless)
+
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.sqldelight)
+}
+
+android {
+    namespace = "tachiyomi.data"
+
+    sqldelight {
+        databases {
+            create("Database") {
+                packageName.set("tachiyomi.data")
+                dialect(libs.sqldelight.sqliteDialect338)
+                schemaOutputDirectory.set(project.file("./src/main/sqldelight"))
+            }
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
+    }
+}
+
+dependencies {
+    implementation(projects.sourceApi)
+    implementation(projects.domain)
+    implementation(projects.core.common)
+
+    api(libs.bundles.sqldelight)
+}
